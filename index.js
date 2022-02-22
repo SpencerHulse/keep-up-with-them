15285193d028b086407eb4f576cf0f76331cd334
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-22 01:43:54 -0500

    Added option to view all roles.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 const cTable = require("console.table");
-
+// Department Functions
 const {
   departmentsHandler,
   addDepartment,
 } = require("./lib/db-functions/departments");
-
+// Role Functions
+const { rolesHandler, addRole } = require("./lib/db-functions/roles");
+// Inquirer Prompts
 const {
   startOptions,
   goAgain,
   newDepartment,
-} = require("./lib/inquirer-questions/questions");
+} = require("./lib/inquirer-questions/prompts");
 
 const startApplication = async () => {
   await inquirer
@@ -39,10 +41,20 @@ const exitApplication = () => {
 const choiceHandler = async ({ options: choice }) => {
   if (choice === "View All Departments") {
     await departmentsHandler();
-  } else if (choice === "Add a Department") {
+  }
+
+  if (choice === "Add a Department") {
     await inquirer
       .prompt(newDepartment)
-      .then((departmentName) => addDepartment(departmentName));
+      .then((choice) => addDepartment(choice));
+  }
+
+  if (choice === "View All Roles") {
+    await rolesHandler();
+  }
+
+  if (choice === "Add a Role") {
+    await addRole();
   }
 };
 
