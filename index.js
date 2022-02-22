901d7eaa54b1e163ebfe92df7dcbffa48dec56b3
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-21 22:50:55 -0500

    Added screen to make choices and the get all departments choice.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const inquirer = require("inquirer");
+const { startOptions } = require("./lib/inquirer-questions/questions");
+const db = require("./db/connection");
+const cTable = require("console.table");
+
+let departmentsData;
+
+const getAllDepartments = async () => {
+  await db
+    .promise()
+    .query(`SELECT * FROM departments`)
+    .then(([rows]) => {
+      console.log(rows);
+      departmentsData = rows;
+    })
+    .catch(console.log);
+};
+
+const departmentsHandler = async () => {
+  await getAllDepartments();
+  console.log(cTable.getTable(departmentsData));
+};
+
+const choiceHandler = async (choice) => {
+  if (choice.options === "View All Departments") {
+    await departmentsHandler();
+  }
+};
+
+inquirer.prompt(startOptions).then((choice) => choiceHandler(choice));
